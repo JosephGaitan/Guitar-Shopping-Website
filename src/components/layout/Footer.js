32b8e5da17f2ac2e67900b614d7c359b96629ae7
Joseph Gaitan <josephopgaitan@gmail.com>
2023-04-03 14:42:34 +0100

    added a footer and a header

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,19 @@
-
-
+import Link from "next/link";
+import styles from "../../styles/footer.module.css";
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <footer className={styles.footer}>
+      <div className={`contenedor ${styles.contenido} `}>
+        <nav className={styles.navegacion}>
+          <Link href="/">Home</Link>
+          <Link href="/aboutUs">About us</Link>
+          <Link href="/shop">Shop Now</Link>
+          <Link href="/blog">Music Blog</Link>
+        </nav>
+        <p className={styles.copyright}>All rights reserved {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,47 @@
-import Image from "next/image"
-import Link from "next/link"
-import styles from "../../styles/header.module.css"
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../styles/header.module.css";
+import { useRouter } from "next/router";
 
 const Header = () => {
-  return (
-   <header className={styles.trin}>
-    <div className="contenedor">
-      <Image  src="/img/logo.svg" width={200} height={10} alt="img"/>
-      <nav>
-      <Link href="/">Home</Link>
-      <Link href="/aboutUs">About us</Link>
-      <Link href="/shop">Shop Now</Link>
-      <Link href="/blog">Music Blog</Link>
-      </nav>
-    </div>
-   </header>
-  )
-}
+  const router = useRouter();
 
-export default Header
+  return (
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
+        <Link href="/">
+          <Image src="/img/logo.svg" width={200} height={10} alt="img" />
+        </Link>
+
+        <nav className={styles.navegacion}>
+          <Link
+            className={router.pathname === "/" ? styles.active : ''}
+            href="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={router.pathname === "/aboutUs" ? styles.active : ''}
+            href="/aboutUs"
+          >
+            About us
+          </Link>
+          <Link
+            className={router.pathname === "/shop" ? styles.active : ''}
+            href="/shop"
+          >
+            Shop Now
+          </Link>
+          <Link
+            className={router.pathname === "/blog" ? styles.active : ''}
+            href="/blog"
+          >
+            Music Blog
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
